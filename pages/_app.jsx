1e9bd9f5c02910a54908fc3957bd3c2789e28f8c
Adetayo <tuneymitch@gmail.com>
2023-08-09 12:08:12 +0100

    feat: connect button setup

---
+++ pages/_app.jsx
@@ -0,0 +1,33 @@
+import '@/styles/globals.css'
+
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { mainnet, polygon, optimism, arbitrum, zora } from 'wagmi/chains'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+
+const { chains, publicClient } = configureChains(
+	[mainnet, polygon, optimism, arbitrum, zora],
+	[alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+)
+const { connectors } = getDefaultWallets({
+	appName: 'Myraid Flow',
+	projectId: 'd50d12ce56fee72d9ec4cafb51cb2e13',
+	chains,
+})
+
+export const wagmiConfig = createConfig({
+	autoConnect: true,
+	connectors,
+	publicClient,
+})
+
+export default function App({ Component, pageProps }) {
+	return (
+		<WagmiConfig config={wagmiConfig}>
+			<RainbowKitProvider chains={chains}>
+				<Component {...pageProps} />
+			</RainbowKitProvider>
+		</WagmiConfig>
+	)
+}
